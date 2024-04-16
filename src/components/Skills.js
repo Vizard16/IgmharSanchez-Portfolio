c6851a3a07fef120f76cd8e254067a2866f67931
Igmhar SC <130608853+Vizard16@users.noreply.github.com>
2024-04-16 00:29:08 -0600

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,32 +1,59 @@
-import meter1 from "../assets/img/meter1.svg";
-import meter2 from "../assets/img/meter2.svg";
-import meter3 from "../assets/img/meter3.svg";
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
-import arrow1 from "../assets/img/arrow1.svg";
-import arrow2 from "../assets/img/arrow2.svg";
-import colorSharp from "../assets/img/color-sharp.png"
+import React from "react";
+import Slider from "react-slick";
+
+import { TechIcon } from "./misc";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import colorSharp from "../assets/img/color-sharp.png";
 
 export const Skills = () => {
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5
+  const techs = [
+    // ... your techs array with image paths and names
+    {
+      src: "/images/langs/svelte.png",
+      padding: "p-10",
+      name: "Svelte",
+      link: "https://svelte.dev",
     },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
+  ];
+
+  const settings = 
+    // ... your settings object from SimpleSlider
+    {
+      dots: false,
+      infinite: true,
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      speed: 5000,
+      autoplaySpeed: 10,
+      cssEase: "linear",
+      vertical: false,
+      pauseOnHover: true,
+      swipeToSlide: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 5,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+      ],
   };
+
 
   return (
     <section className="skill" id="skills">
@@ -36,29 +63,22 @@ export const Skills = () => {
                     <div className="skill-bx wow zoomIn">
                         <h2>Skills</h2>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
-                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter2} alt="Image" />
-                                <h5>Brand Identity</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>Logo Design</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
-                            </div>
-                        </Carousel>
+                        <Slider {...settings} className="mx-6 w-full -mt-2">
+                          {techs.map((tech) => (
+                            <TechIcon
+                              key={tech.name}
+                              src={tech.src}
+                              padding={tech.padding}
+                              name={tech.name}
+                              link={tech.link}
+                            />
+                          ))}
+                        </Slider>
                     </div>
                 </div>
             </div>
         </div>
         <img className="background-image-left" src={colorSharp} alt="Image" />
     </section>
-  )
-}
+  );
+};
