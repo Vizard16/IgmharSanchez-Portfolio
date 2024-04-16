ab862f56dbd6a5b9e10f075c6ff47330dc23fafc
Igmhar SC <130608853+Vizard16@users.noreply.github.com>
2024-04-16 01:54:10 -0600

    Made a carrousel of skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,84 +1,97 @@
 import React from "react";
-import Slider from "react-slick";
-
-import { TechIcon } from "./misc";
-
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import { Carousel } from 'react-bootstrap';
 import colorSharp from "../assets/img/color-sharp.png";
+import { TechIcon } from "./misc"; // Ensure this imports correctly as per your project structure
 
 export const Skills = () => {
   const techs = [
-    // ... your techs array with image paths and names
-    {
-      src: "/images/langs/svelte.png",
-      padding: "p-10",
-      name: "Svelte",
-      link: "https://svelte.dev",
-    },
+    { src: "/images/langs/svelte.png", name: "Svelte", link: "https://svelte.dev" },
+    { src: "https://skillicons.dev/icons?i=git", name: "Svelte", link: "https://svelte.dev" },
+    // Add more items as needed
   ];
 
-  const settings = 
-    // ... your settings object from SimpleSlider
+  const slidesData = [
     {
-      dots: false,
-      infinite: true,
-      slidesToShow: 6,
-      slidesToScroll: 1,
-      autoplay: true,
-      speed: 5000,
-      autoplaySpeed: 10,
-      cssEase: "linear",
-      vertical: false,
-      pauseOnHover: true,
-      swipeToSlide: true,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 5,
-          },
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 3,
-          },
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 2,
-          },
-        },
-      ],
-  };
-
+        id: 1,
+        images: [
+            { src: "/images/langs/svelte.png", alt: "Svelte", name: "Svelte",  link: "https://svelte.dev" },
+            { src: "https://skillicons.dev/icons?i=git", alt: "React", name: "React" },
+            { src: "/images/langs/vue.png", alt: "Vue", name: "Vue" },
+            { src: "https://skillicons.dev/icons?i=git", alt: "React", name: "React" }
+        ]
+    },
+    {
+        id: 2,
+        images: [
+            { src: "/images/langs/angular.png", alt: "Angular", name: "Angular" },
+            { src: "/images/langs/ember.png", alt: "Ember", name: "Ember" },
+            { src: "/images/langs/backbone.png", alt: "Backbone", name: "Backbone" }
+        ]
+    },
+    {
+        id: 3,
+        images: [
+            { src: "/images/langs/django.png", alt: "Django", name: "Django" },
+            { src: "/images/langs/flask.png", alt: "Flask", name: "Flask" },
+            { src: "/images/langs/ruby.png", alt: "Ruby on Rails", name: "Rails" }
+        ]
+    }
+];
 
   return (
     <section className="skill" id="skills">
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <div className="skill-bx wow zoomIn">
-                        <h2>Skills</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
-                        <Slider {...settings} className="mx-6 w-full -mt-2">
-                          {techs.map((tech) => (
-                            <TechIcon
-                              key={tech.name}
-                              src={tech.src}
-                              padding={tech.padding}
-                              name={tech.name}
-                              link={tech.link}
-                            />
-                          ))}
-                        </Slider>
-                    </div>
-                </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="skill-bx wow zoomIn">
+              <h3>Programming Languages</h3>
+              <Carousel interval={3000}> {/* Auto-cycle every 3000 ms */}
+                  {slidesData.map((slide) => (
+                      <Carousel.Item key={slide.id}>
+                          <div className="d-flex justify-content-center">
+                              {slide.images.map((image) => (
+                                  <div key={image.name} className="p-2">
+                                      <TechIcon src={image.src} name={image.name} link={image.link} />
+                                  </div>
+                              ))}
+                          </div>
+                      </Carousel.Item>
+                  ))}
+              </Carousel>
+              
+              
             </div>
+          </div>
         </div>
-        <img className="background-image-left" src={colorSharp} alt="Image" />
+      </div>
+      <img className="background-image-left" src={colorSharp} alt="Image" />
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="skill-bx wow zoomIn">
+              <h3>Technologies</h3>
+              <Carousel interval={3000}> {/* Auto-cycle every 3000 ms */}
+                  {slidesData.map((slide) => (
+                      <Carousel.Item key={slide.id}>
+                          <div className="d-flex justify-content-center">
+                              {slide.images.map((image) => (
+                                  <div key={image.name} className="p-2">
+                                      <TechIcon src={image.src} name={image.name} link={image.link} />
+                                  </div>
+                              ))}
+                          </div>
+                      </Carousel.Item>
+                  ))}
+              </Carousel>
+              
+              
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
+    
   );
 };
+
+//ACUERDATE LO DE LAS SECTIONSSSS
