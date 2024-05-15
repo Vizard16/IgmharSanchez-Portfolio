d8e591bdf3173ffeeed39b91db775e93830f76cb
Igmhar SC <130608853+Vizard16@users.noreply.github.com>
2024-05-15 17:26:51 -0600

    Update About.js
    Made better description

--- src/components/About.js
+++ src/components/About.js
@@ -11,6 +11,7 @@ export const About = () => {
 
   return (
     <section className="about" id="about">
+
       <Container>
         <Row>
           <Col size={12}>
@@ -19,26 +20,35 @@ export const About = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2>About Me</h2>
                 
-                
               </div>}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2}></img>
-
+      
+      {/* Background blur effect */}
+      
       {/* Apply the terminal window */}
-      <div className="fakeMenu">
+      <div className="fakeMenu" style={{ marginTop: '20px' }}>
         <div className="fakeButtons fakeClose"></div>
         <div className="fakeButtons fakeMinimize"></div>
         <div className="fakeButtons fakeZoom"></div>
       </div>
-      <div className="fakeScreen" style={{ height: `${screenSize}px` }}>
+    
+
+      <div className="fakeScreen" >
         <p className="line1">$ sudo chmod About Igmhar<span className="cursor1">_</span></p>
         <p className="line2"> I'm an enthusiast about programming, robotics, automation. <span className="cursor2">_</span></p>
-        <p className="line3">Very passionate and curious person who loves learning about new technologies and exploring their possibilities! <span className="cursor3">_</span></p>
+        <p className="line3">I'm a proficient and passionate engineering student with experience in team-based software development and robotics, always eager to learn and self-driven towards solving world problems. <span className="cursor3">_</span></p>
         <p className="line4"><span className="cursor4">_</span></p>
       </div>
+
+      <span className="background-blur-about"></span>
+
+      
+
+    
+      
     </section>
   )
 }
